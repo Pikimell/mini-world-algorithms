f376cef2634cf26390c64d5f2e7b31087a74e5bb
Volodymyr Pashchenko <volodkaposhta@gmail.com>
2024-09-04 23:52:28 +0300

    update

--- src/js/atom.js
+++ src/js/atom.js
@@ -1,14 +1,31 @@
 class Atom {
-  constructor({ x, y, color, velocity, size = 10 }) {
+  static items = [];
+
+  constructor({ x, y, color, velocity, size = 10, group = 0 }) {
     this.x = x;
-    this.y = x;
+    this.y = y;
+    this.vx = 0;
+    this.vy = 0;
     this.color = color;
     this.x = x;
     this.size = 10;
+
+    if (!Array.isArray(Atom.items[group])) {
+      Atom.items[group] = [];
+    }
+    Atom.items[group].push(this);
   }
 
   draw() {
     fill(this.color);
     ellipse(this.x, this.y, this.size);
+  }
+
+  static draw() {
+    for (const group of Atom.items) {
+      for (const atom of group) {
+        atom.draw();
+      }
+    }
   }
 }
--- src/js/index.js
+++ src/js/index.js
@@ -1,103 +1,72 @@
 const WIDTH = 600;
 const HEIGHT = 600;
-const AGENT_SIZE = 10;
+const ATOM_SIZE = 2;
 
-const LEN_X = WIDTH / AGENT_SIZE;
-const LEN_Y = HEIGHT / AGENT_SIZE;
+const LEN_X = WIDTH / ATOM_SIZE;
+const LEN_Y = HEIGHT / ATOM_SIZE;
 
-const EMPTY_PERCENT = 0.01;
-const EMPTY_COUNT = Math.round(LEN_X * LEN_Y * EMPTY_PERCENT);
-const EMPTY_POINTS = Array.from({ length: EMPTY_COUNT });
-
-const FRAME_RATE = 1;
+const FRAME_RATE = 5;
 
 let START_GAME = true;
-
-Agent.world = [];
-Agent.empty = EMPTY_POINTS;
 
 function setup() {
   // frameRate(FRAME_RATE);
   createCanvas(WIDTH, HEIGHT);
-  background(255);
+  background(0);
   setWorld();
 }
 
 function setWorld() {
-  const length = Math.floor((LEN_X * LEN_Y) / 2);
-  const first = Array.from({ length }).fill(0);
-  const second = Array.from({ length }).fill(1);
-
-  const res = [...first, ...second];
-  for (let y = 0; y < LEN_Y; y++) {
-    Agent.world[y] = [];
-    for (let x = 0; x < LEN_X; x++) {
-      const type = Math.random();
-      const options = {
-        x,
-        y,
-        size: AGENT_SIZE,
-        type: type > 0.5 ? res.pop() : res.shift(),
-      };
-      const agent = new Agent(options);
-      Agent.world[y].push(agent);
-    }
-  }
-
-  for (let i = 0; i < EMPTY_POINTS.length; i++) {
-    let x, y;
-    do {
-      x = getRandom(2, LEN_X - 1);
-      y = getRandom(2, LEN_Y - 1);
-    } while (Agent.world[y][x] === undefined);
-
-    Agent.world[y][x] = undefined;
-    EMPTY_POINTS[i] = { x, y };
+  for (let i = 0; i < 2; i++) {
+    const x = getRandom(0, 500);
+    const y = getRandom(0, 500);
+    new Atom({ x, y, color: 'red', size: ATOM_SIZE, group: 0 });
   }
 }
 
 function clearCanvas() {
-  fill(255);
-  beginShape();
-  vertex(0, 0);
-  vertex(0, HEIGHT);
-  vertex(WIDTH, HEIGHT);
-  vertex(WIDTH, 0);
-  endShape();
-}
-
-function showAgents() {
-  for (const row of Agent.world) {
-    for (const agent of row) {
-      agent?.show();
-    }
-  }
-
-  for (let i = 0; i < 100; i++) {
-    agentRelocate();
-  }
-}
-
-function agentRelocate() {
-  let x, y;
-  do {
-    x = getRandom(0, LEN_X - 1);
-    y = getRandom(0, LEN_Y - 1);
-  } while (Agent.world[y][x] === undefined);
-
-  const agent = Agent.world[y][x];
-
-  agent?.relocate();
-  agent?.show();
+  fill(0);
+  square(0, 0, WIDTH, HEIGHT);
 }
 
 function draw() {
   if (!START_GAME) return;
-  showAgents();
+  clearCanvas();
+  rule(Atom.items[0], Atom.items[0], -1);
+  Atom.draw();
+}
 
-  for (let item of Agent.empty) {
-    const x = item.x;
-    const y = item.y;
+function rule(firstAtomGroup, secondAtomGroup, g) {
+  for (let i = 0; i < firstAtomGroup.length; i++) {
+    let fx = 0;
+    let fy = 0;
+    const atomA = firstAtomGroup[i];
+
+    for (let j = 0; j < secondAtomGroup.length; j++) {
+      const atomB = secondAtomGroup[j];
+      const dx = atomA.x - atomB.x;
+      const dy = atomA.y - atomB.y;
+      const d = Math.sqrt(dx * dx + dy * dy);
+
+      if (d > 0) {
+        const F = (g * 1) / d;
+        fx += F * dx;
+        fy += F * dy;
+      }
+    }
+
+    atomA.vx = atomA.vx + fx;
+    atomA.vy = atomA.vy + fy;
+
+    atomA.x += atomA.vx;
+    atomA.y += atomA.vy;
+
+    if (atomA.x < 0 || atomA.x > WIDTH) {
+      arguments.vx *= -1;
+    }
+    if (atomA.y < 0 || atomA.y > HEIGHT) {
+      arguments.vx *= -1;
+    }
   }
 }
 
